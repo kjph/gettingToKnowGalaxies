4b90e42c6ec10228d68f0743121895f31b229fe3
kjph <jme.ph2@gmail.com>
2015-03-10 22:06:36 +0800

    updated skeleton for generating regions

--- supernovae/lib/overlayDS9/generateRegions.py
+++ supernovae/lib/overlayDS9/generateRegions.py
@@ -7,9 +7,13 @@
 #close sn-pos file
 
 #write/open region file header
+#"global color=green font="helvetica 20 normal roman" wcs=wcs"
+#"j2000; circle
 
 #write coordinates
+#(on same line)
 
 #write footer
+# text = {#SN_NAME}
 
 #close file
